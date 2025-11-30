53acea77ce4d3aad460b634d34d00e4d2fa9c2b0
KasumiMercury <naoki_shira@outlook.jp>
2025-11-30 22:45:53 +0900

    fix(task): fix user ID error messages

--- internal/task/domain/user/errors.go
+++ internal/task/domain/user/errors.go
@@ -3,7 +3,7 @@ package user
 import "errors"
 
 var (
-	ErrIDGeneration    = errors.New("failed to generate task ID")
-	ErrIDInvalidFormat = errors.New("task ID must be a valid UUID")
-	ErrIDInvalidV7     = errors.New("task ID must be a UUIDv7")
+	ErrIDGeneration    = errors.New("failed to generate user ID")
+	ErrIDInvalidFormat = errors.New("user ID must be a valid UUID")
+	ErrIDInvalidV7     = errors.New("user ID must be a UUIDv7")
 )
