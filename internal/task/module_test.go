bc3f66349f50dfe229a50436ee623b3d0e826c73
KasumiMercury <naoki_shira@outlook.jp>
2025-11-30 21:37:33 +0900

    style: formatting

--- internal/task/module_test.go
+++ internal/task/module_test.go
@@ -93,6 +93,7 @@ func TestNewHTTPHandlerWithRepositoriesError(t *testing.T) {
 			ctx: func() context.Context {
 				ctx, cancel := context.WithCancel(context.Background())
 				cancel()
+
 				return ctx
 			}(),
 			expectError: false,
